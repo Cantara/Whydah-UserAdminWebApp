a05e7760b4d60493071b1f880992c9fbe4fecc63
Thor Henning Hetland <totto@totto.org>
2015-11-18 23:20:38 +0100

    last attempt tonight :)

--- src/main/resources/js/ApplicationCtrl.js
+++ src/main/resources/js/ApplicationCtrl.js
@@ -29,7 +29,7 @@ UseradminApp.controller('ApplicationCtrl', function($scope, $http, $routeParams,
 
   $scope.activateApplicationDetail = function(id) {
     console.log('Activating application detail...', id);
-    applicationId = 100;
+    id = 100;
     Applications.get(id, function(){
         //$scope.form.userDetail.$setPristine();
         $('#applicationdetail').modal('show');
