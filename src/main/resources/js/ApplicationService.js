dddf0d16bec979dda440964e382c63b64f39471b
baardl <bard.lind@gmail.com>
2015-11-21 15:20:03 +0100

    #35 Posting Json content ok.

--- src/main/resources/js/ApplicationService.js
+++ src/main/resources/js/ApplicationService.js
@@ -97,7 +97,6 @@ UseradminApp.service('Applications', function($http,Messages){
             that.application = data;
             that.application.secret = data.security.secret;
             that.application.applicationJson = JSON.stringify(data);
-            //that.application.secret = data.applicationC
             if(callback) {
                 callback(data);
             }
@@ -108,82 +107,106 @@ UseradminApp.service('Applications', function($http,Messages){
     this.add = function(application, successCallback) {
         console.log('Adding application', application);
         var that = this;
-        application.security = {};
-        application.security.secret = application.secret;
-        delete application.secret;
-        $http({
-            method: 'POST',
-            url: baseUrl+'application/',
-            data: application
-        }).success(function (data) {
-            Messages.add('success', 'application "'+application.name+'" was added successfully.');
-            application.id = data.id;
-            that.search(that.searchQuery);
-            if(successCallback){
-                successCallback();
-            }
-        }).error(function(data,status){
-            console.log('application was not added', data);
-            switch (status) {
-                case 403:
-                    Messages.add('danger', 'application was not added! No access...');
-                    break;
-                case 404:  /* 404 No access */
-                    Messages.add('danger', 'application was not added! No access...');
-                    break;
-                case 409:  /* 409 Conflict - user exists or was double posted */
-                    Messages.add('danger', 'application was not added! Already exists...');
-                    break;
-                default:
-                    Messages.add('danger', 'application was not added and! Try again later...');
-            }
-            $scope.activateTimeoutModal();
-        });
+
+        //application.security = {};
+        //application.security.secret = application.secret;
+        //delete application.secret;
+        var postData = buildApplicationUpdate(application);
+        if (postData.hasOwnProperty('name')) {
+            $http({
+                method: 'POST',
+                url: baseUrl + 'application/',
+                data: postData
+            }).success(function (data) {
+                Messages.add('success', 'application "' + application.name + '" was added successfully.');
+                application.id = data.id;
+                that.search(that.searchQuery);
+                if (successCallback) {
+                    successCallback();
+                }
+            }).error(function (data, status) {
+                console.log('application was not added', data);
+                switch (status) {
+                    case 403:
+                        Messages.add('danger', 'application was not added! No access...');
+                        break;
+                    case 404:  /* 404 No access */
+                        Messages.add('danger', 'application was not added! No access...');
+                        break;
+                    case 409:  /* 409 Conflict - user exists or was double posted */
+                        Messages.add('danger', 'application was not added! Already exists...');
+                        break;
+                    default:
+                        Messages.add('danger', 'application was not added and! Try again later...');
+                }
+                $scope.activateTimeoutModal();
+            });
+        }
         return this;
     };
 
     this.save = function(application, successCallback) {
         console.log('Updating application', JSON.stringify(application));
         var that = this;
-        if (application.hasOwnProperty('secret')) {
-            if (application.hasOwnProperty('security')) {
-                application.security.secret = application.secret;
-            } else {
-                application.security = {};
-                application.security.secret = application.secret;
-            }
-            delete application.secret;
+        var postData = buildApplicationUpdate(application);
+        if (postData.hasOwnProperty('name')) {
+            $http({
+                method: 'PUT',
+                url: baseUrl + 'application/' + application.id + '/',
+                data: postData
+            }).success(function (data) {
+                Messages.add('success', 'application "' + application.name + '" was updated successfully.');
+                application.id = data.id;
+                that.search(that.searchQuery);
+                if (successCallback) {
+                    successCallback();
+                }
+            }).error(function (data, status) {
+                console.log('application was not updated', data);
+                switch (status) {
+                    case 400:
+                        Messages.add('danger','application was not updated! Please validate your form and applicationjson input.');
+                        break;
+                    case 403:
+                        Messages.add('danger', 'application was not updated! No access...');
+                        break;
+                    case 404:  /* 404 No access */
+                        Messages.add('danger', 'application was not updated! No access...');
+                        break;
+                    case 409:  /* 409 Conflict - user exists or was double posted */
+                        Messages.add('danger', 'application was not updated! Already exists...');
+                        break;
+                    default:
+                        Messages.add('danger', 'application was not updated and! Try again later...');
+                }
+                $scope.activateTimeoutModal();
+            });
         }
-        $http({
-            method: 'PUT',
-            url: baseUrl+'application/'+application.id +'/',
-            data: application
-        }).success(function (data) {
-            Messages.add('success', 'application "'+application.name+'" was updated successfully.');
-            application.id = data.id;
-            that.search(that.searchQuery);
-            if(successCallback){
-                successCallback();
-            }
-        }).error(function(data,status){
-            console.log('application was not updated', data);
-            switch (status) {
-                case 403:
-                    Messages.add('danger', 'application was not updated! No access...');
-                    break;
-                case 404:  /* 404 No access */
-                    Messages.add('danger', 'application was not updated! No access...');
-                    break;
-                case 409:  /* 409 Conflict - user exists or was double posted */
-                    Messages.add('danger', 'application was not updated! Already exists...');
-                    break;
-                default:
-                    Messages.add('danger', 'application was not updated and! Try again later...');
-            }
-            $scope.activateTimeoutModal();
-        });
         return this;
     };
+
+    function buildApplicationUpdate(application) {
+        var postData = {};
+        if (application.hasOwnProperty('applicationJson')) {
+            try {
+                postData = JSON.parse(application.applicationJson);
+            } catch (e) {
+                Messages.add('danger', 'Could not parse applicationJson to JSON object.');
+            }
+        } else {
+            if (application.hasOwnProperty('secret')) {
+                if (application.hasOwnProperty('security')) {
+                    application.security.secret = application.secret;
+                } else {
+                    application.security = {};
+                    application.security.secret = application.secret;
+                }
+                delete application.secret;
+            }
+            postData = application;
+        }
+        return postData;
+    }
 
     this.delete = function(application, successCallback) {
         console.log('Deleting application', JSON.stringify(application));
