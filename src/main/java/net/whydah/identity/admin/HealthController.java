fed2a6c604093f4480c79ba4846145d67067e878
totto <totto@totto.org>
2016-05-27 12:13:39 +0200

    added status and health

--- src/main/java/net/whydah/identity/admin/HealthController.java
+++ src/main/java/net/whydah/identity/admin/HealthController.java
@@ -1,28 +1,17 @@
 package net.whydah.identity.admin;
 
-import com.sun.jersey.api.client.Client;
-import net.whydah.identity.admin.config.AppConfig;
 import net.whydah.sso.util.WhydahUtil;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.util.Properties;
 
+@RequestMapping("/health")
 @Controller
 public class HealthController {
     private static final Logger log = LoggerFactory.getLogger(HealthController.class);
@@ -33,9 +22,8 @@ public class HealthController {
 
     }
 
-    @GET
-    @Path("/health")
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN + ";charset=utf-8")
+    @RequestMapping("/")
     public Response isHealthy() {
         boolean ok = true;
         String statusText = WhydahUtil.getPrintableStatus(tokenServiceClient.getWAS());
