99da001f9b5ae8552406ff620626e45a2c3fcf91
totto <totto@totto.org>
2018-04-11 14:37:25 +0200

    Attempt to handle empty applicationid issue

--- src/main/resources/js/ApplicationCtrl.js
+++ src/main/resources/js/ApplicationCtrl.js
@@ -168,12 +168,7 @@ UseradminApp.controller('ApplicationCtrl', function($scope, $http, $window, $rou
 
 	$scope.newApplicationDetail = function() {
 		Applications.application = {isNew: true};
-        Applications.application.id=function() {
-                                      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                                        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-                                        return v.toString(16);
-                                      });
-                                    };
+        Applications.application.id=uuid();
 		Applications.application.tagList=[];
 		Applications.application.organizationNames=[];
 		Applications.application.roles=[];
--- src/main/resources/js/lib/md5.js
+++ src/main/resources/js/lib/md5.js
@@ -377,3 +377,10 @@ function bit_rol(num, cnt)
 {
   return (num << cnt) | (num >>> (32 - cnt));
 }
+
+function uuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
